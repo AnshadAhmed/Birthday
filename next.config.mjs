cefcee473a872ec5c86651dd97465058b39446c8
Anshad <anshad1216@gmail.com>
2025-12-19 10:13:29 +0530

    Remove basePath/assetPrefix - serve from gh-pages root

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: 'export',
-	basePath: '/Birthday',
-	assetPrefix: '/Birthday',
-	trailingSlash: true,
+  output: 'export',
+  trailingSlash: true,
 };
 
 export default nextConfig;
